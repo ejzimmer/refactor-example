130a0c3f532ceaad6f32d039801a55a7c29152eb
Erin Zimmer <ejzimmer@gmail.com>
2022-02-03 13:39:05 +1100

    rearrange things

--- src/__tests__/exampleTests.test.jsx
+++ src/__tests__/exampleTests.test.jsx
@@ -1,11 +1,43 @@
 import moment from "moment"
 import { useEffect, useState } from "react"
+import { format } from "date-fns"
+import { render } from "@testing-library/react"
 
 function formatDate(date) {
   if (!date) return ""
 
-  return moment(new Date(date)).format("yyyy-MM-D")
+  try {
+    return format(new Date(date), "yyyy-MM-d")
+  } catch (e) {
+    return "Invalid date"
+  }
 }
+
+describe("formatDate function", () => {
+  it("formats a date", () => {
+    expect(formatDate(new Date("2022-02-02"))).toBe("2022-02-2")
+  })
+
+  it("formats a valid string", () => {
+    expect(formatDate("2022-02-02")).toBe("2022-02-2")
+  })
+  it("formats a string which isn't a date", () => {
+    expect(formatDate("not a date")).toBe("Invalid date")
+  })
+  it("formats a string which is an invalid date", () => {
+    expect(formatDate("2022-02-30")).toBe("2022-03-2")
+  })
+
+  it("formats a number", () => {
+    expect(formatDate(345690097)).toBe("1970-01-5")
+  })
+  it("formats a negative number", () => {
+    expect(formatDate(-345990067)).toBe("1969-12-27")
+  })
+  it("formats 0", () => {
+    expect(formatDate(0)).toBe("")
+  })
+})
 
 function UserInfo({ user }) {
   const date = formatDate(user.birthday)
@@ -17,6 +49,24 @@ function UserInfo({ user }) {
     </div>
   )
 }
+
+describe("UserInfo component", () => {
+  it("displays the username and birthday", () => {
+    const { container } = render(
+      <UserInfo user={{ name: "Bob", birthday: new Date("2010-02-10") }} />
+    )
+
+    expect(container.textContent).toBe("Bob was born on 2010-02-10 (Wednesday)")
+  })
+
+  it("matches the snapshot", () => {
+    const { container } = render(
+      <UserInfo user={{ name: "Bob", birthday: new Date("2010-02-11") }} />
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+})
 
 function Increment({ initialNumber }) {
   const [number, setNumber] = useState(initialNumber)
