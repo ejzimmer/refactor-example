a7a4e78d9d69860510654ec0e9001de090a444fa
Erin Zimmer <ejzimmer@gmail.com>
2022-02-10 10:56:48 +1100

    refactor ProductTypeSelector

--- src/ProductTypeSelector.jsx
+++ src/ProductTypeSelector.jsx
@@ -1,34 +1,33 @@
 import React from "react"
 
+const productNames = {
+  SEED_PACKETS: "Seed Packets",
+  SEEDLINGS: "Seedlings",
+}
+
 export function ProductTypeSelector({ onChange, value }) {
   const handleChange = (event) => {
     const productType = event.currentTarget.value
     onChange(productType)
   }
 
+  const productTypes = Object.entries(productNames)
+
   return (
     <fieldset style={{ display: "flex", gap: "1.5em" }}>
       <legend>Product type</legend>
-      <label>
-        <input
-          type="radio"
-          name="product_type"
-          value="SEED_PACKETS"
-          checked={value === "SEED_PACKETS"}
-          onChange={handleChange}
-        />
-        Seed Packets
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="product_type"
-          value="SEEDLINGS"
-          checked={value === "SEEDLINGS"}
-          onChange={handleChange}
-        />
-        Seedlings
-      </label>
+      {productTypes.map(([key, label]) => (
+        <label key={key}>
+          <input
+            type="radio"
+            name="product_type"
+            value={key}
+            checked={value === key}
+            onChange={handleChange}
+          />
+          {label}
+        </label>
+      ))}
     </fieldset>
   )
 }
