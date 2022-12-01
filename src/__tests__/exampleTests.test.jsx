27c3a9485f62527a7a806099f5f6581748620318
Erin Zimmer <ezimmer@squareup.com>
2022-12-01 12:20:07 +1100

    characterisation tests

--- src/__tests__/exampleTests.test.jsx
+++ src/__tests__/exampleTests.test.jsx
@@ -7,6 +7,56 @@ function formatDate(date) {
   return moment(new Date(date)).format("yyyy-MM-D")
 }
 
+describe('formatDate', () => {
+  describe('when nothing is passed in', () => {
+    it('returns an empty string', () => {
+      expect(formatDate()).toBe('')
+    })
+  })
+
+  describe('when a Date is passed in', () => {
+    it('formats the date', () => {
+      expect(formatDate(new Date('2001-01-01'))).toBe('2001-01-1')
+    })
+  })
+
+  describe('when a date string is passed in', () => {
+    it('formats the date', () => {
+      expect(formatDate('2001-01-01')).toBe('2001-01-1')
+    })    
+  })
+
+  describe('when an invalid date string is passed in', () => {
+    it('formats the date', () => {
+      expect(formatDate('2001-02-31')).toBe('2001-03-3')
+    })
+  })
+
+  describe('when a non-date string is passed in', () => {
+    it('returns an error string', () => {
+      expect(formatDate('banana')).toBe('Invalid date')
+    })
+  })
+
+  describe('when a number is passed in', () => {
+    it('formats the date', () => {
+      expect(formatDate(new Date('2001-01-01').getTime())).toBe('2001-01-1')
+    })    
+  })
+
+  describe('when a negative number is passed in', () => {
+    it('formats the date', () => {
+      expect(formatDate(new Date('1960-01-01').getTime())).toBe('1960-01-1')
+    })    
+  })
+
+  describe('when 0 is passed in', () => {
+    it('returns empty string', () => {
+      expect(formatDate(0)).toBe('')
+    })
+  })
+})
+
 function UserInfo({ user }) {
   const date = formatDate(user.birthday)
   const day = moment(user.birthday).format("dddd")
@@ -17,3 +67,10 @@ function UserInfo({ user }) {
     </div>
   )
 }
+
+describe('UserInfo component', () => {
+  it('displays the user\'s info', () => {
+    const { container } = render(<UserInfo user={{ name: 'Bob', birthday: '1990-01-01'}} />)
+    expect(container).toHaveTextContent('Bob was born on 1990-01-1 (Monday)')
+  })
+})
